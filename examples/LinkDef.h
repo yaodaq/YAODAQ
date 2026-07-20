863c11785dd58122a9d4e954d71e5e80858a8a2f
flagarde <flagarde.programs@pm.me>
2026-07-20 14:37:52 +0200

    add clang-format

--- examples/LinkDef.h
+++ examples/LinkDef.h
@@ -1,16 +1,18 @@
 #pragma once
+// clang-format off
 #ifdef __CLING__
-  #pragma link C++ class DCT::RawData + ;
-  #pragma link C++ class std::vector < DCT::RawData> + ;
+  #pragma link C++ class DCT::RawData+;
+  #pragma link C++ class std::vector<DCT::RawData>+;
 
-  #pragma link C++ class DCT::DecodedRawData + ;
-  #pragma link C++ class std::vector < DCT::DecodedRawData> + ;
+  #pragma link C++ class DCT::DecodedRawData+;
+  #pragma link C++ class std::vector<DCT::DecodedRawData>+;
 
-  #pragma link C++ class DCT::IntermediateEvent + ;
-  #pragma link C++ class std::vector < DCT::IntermediateEvent> + ;
+  #pragma link C++ class DCT::IntermediateEvent+;
+  #pragma link C++ class std::vector<DCT::IntermediateEvent>+;
 
-  #pragma link C++ class DCT::Hit + ;
-  #pragma link C++ class DCT::Event + ;
-  #pragma link C++ class std::vector < DCT::Hit> + ;
-  #pragma link C++ class std::vector < DCT::Event> + ;
+  #pragma link C++ class DCT::Hit+;
+  #pragma link C++ class DCT::Event+;
+  #pragma link C++ class std::vector<DCT::Hit>+;
+  #pragma link C++ class std::vector<DCT::Event>+;
 #endif
+// clang-format on
