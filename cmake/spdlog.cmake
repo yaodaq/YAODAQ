97c63b578b52300c0025d25332faaf5fe590d851
flagarde <flagarde.programs@pm.me>
2026-09-17 11:48:00 +0200

    fix spdlog

--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -18,7 +18,7 @@ CPMAddPackage(NAME spdlog
               GIT_REPOSITORY ${SPDLOG_REPOSITORY}
               GIT_TAG ${SPDLOG_TAG}
               OPTIONS "SPDLOG_BUILD_ALL OFF"
-                      "SPDLOG_BUILD_SHARED ON"
+                      "SPDLOG_BUILD_SHARED OFF"
                       "SPDLOG_BUILD_PIC ON"
                       "SPDLOG_ENABLE_PCH OFF"
                       "SPDLOG_BUILD_EXAMPLE OFF"
